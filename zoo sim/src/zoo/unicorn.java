62ac5f2c02a08c672f02f45d87e0ceabc675c47e
randomice1219 <andersoncc2@appstate.edu>
2025-10-06 19:27:24 -0400

    balls

--- zoo sim/src/zoo/unicorn.java
+++ zoo sim/src/zoo/unicorn.java
@@ -1,15 +1,3 @@
-<<<<<<< HEAD:zoo sim/src/app/unicorn.java
-package zoo sim.src.app;
-
-public class unicorn extends animal{
-    String eat(){
-        return "";
-    }
-    String move(){
-        return "";
-    }
-    String sound(){
-=======
 package zoo;
 
 public class unicorn extends animal{
@@ -23,7 +11,6 @@ public class unicorn extends animal{
     }
     @Override
     public String sound(){
->>>>>>> 269751b3fb3e2640c283822ed074ca5eb49cac59:zoo sim/src/zoo/unicorn.java
         return "";
     }
 }
