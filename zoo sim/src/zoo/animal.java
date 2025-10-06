08dccac650c7934cd7e0a2c36e8ca8936c30d5d3
randomice1219 <andersoncc2@appstate.edu>
2025-10-06 19:23:28 -0400

    hgygyuy

--- zoo sim/src/zoo/animal.java
+++ zoo sim/src/zoo/animal.java
@@ -1,8 +1,5 @@
-<<<<<<< HEAD:zoo sim/src/app/animal.java
-=======
 package zoo;
 
->>>>>>> 269751b3fb3e2640c283822ed074ca5eb49cac59:zoo sim/src/zoo/animal.java
 public abstract class animal{
     // methods inherited by the animals
     abstract String eat();
