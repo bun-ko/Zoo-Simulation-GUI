7a59049201d03b54b865e24c1fe82f434850cca0
randomice1219 <andersoncc2@appstate.edu>
2025-10-08 13:00:27 -0400

    manmade horrors beyond human comprehension

--- zoo sim/src/zoo/giraffe.java
+++ zoo sim/src/zoo/giraffe.java
@@ -2,15 +2,7 @@ package zoo;
 
 public class giraffe extends animal
 {
-    public String eat(){
-        return "Eating leaves from tall trees...";
-    }
-    @Override
-    public String move(){
-        return "Walking on all fours...";
-    }
-    @Override
-    public String sound(){
-        return "Humming, snorting, grunting...";
+    public giraffe(){
+        eatBehavior = new herbivoreEat();
     }
 }
